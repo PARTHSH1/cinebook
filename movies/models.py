cb3225ed189c54e2d27e3fecc028c913eceed328
PARTHSH1 <110349699+PARTHSH1@users.noreply.github.com>
2026-04-01 05:23:13 +0530

    Add ordering to Genre model
    Add ordering to Genre model by name.

--- movies/models.py
+++ movies/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-
 class Genre(models.Model):
     name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ['name']  # add this
 
     def __str__(self):
         return self.name
