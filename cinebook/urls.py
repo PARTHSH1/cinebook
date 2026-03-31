12a3a4c2b49ca46b95812d69a5fefe28ee0f7c1e
PARTHSH1 <110349699+PARTHSH1@users.noreply.github.com>
2026-04-01 05:10:57 +0530

    Add static media URL handling in debug mode

--- cinebook/urls.py
+++ cinebook/urls.py
@@ -31,3 +31,6 @@ urlpatterns = [
     path('api/bookings/', include('bookings.urls')),
     path('api/payments/', include('payments.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
